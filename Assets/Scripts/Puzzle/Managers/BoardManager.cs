f087ea2e322d763b264e07f4cccde6aac8ecc5c0
Veysel Er <veyseler.cs.ist@gmail.com>
2022-02-07 22:04:17 +0300

    input manager added

--- Assets/Scripts/Puzzle/Managers/BoardManager.cs
+++ Assets/Scripts/Puzzle/Managers/BoardManager.cs
@@ -7,22 +7,37 @@ public class BoardManager : MonoBehaviour
     [SerializeField] private GameObject _boardParent;
     [SerializeField] private RectTransform _boardRectTransform;
 
+    private InputManager _inputManager;
+    
+    public static BoardManager Instance;
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
+        //init references
+        _inputManager = InputManager.Instance;
+        
         //set the size for grid layout
         var boardSquare = Instantiate(_boardSquarePrefab, _boardParent.transform);
         var boardSquareRT = boardSquare.GetComponent<RectTransform>();
-
-        _boardRectTransform.sizeDelta = boardSquareRT.sizeDelta * 9;
+        var boardSquareSize = boardSquareRT.sizeDelta;
+        
+        _boardRectTransform.sizeDelta = boardSquareSize * 9;
         Destroy(boardSquare);
         
+        //init board
         for (int i = 0; i < 9; i++)
         {
             for (int k = 0; k < 9; k++)
             {
                 boardSquare = Instantiate(_boardSquarePrefab, _boardParent.transform);
-                boardSquareRT = boardSquare.GetComponent<RectTransform>();
             }
         }
+        
+        _inputManager.SetBoardSquareSize(boardSquareSize);
     }
 }
--- Assets/Scripts/Puzzle/Managers/InputManager.cs
+++ Assets/Scripts/Puzzle/Managers/InputManager.cs
@@ -1,7 +1,58 @@
-namespace Puzzle.Managers
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
 {
-    public class InputManager
+    private BoardManager _boardManager;
+    
+    public static InputManager Instance;
+    
+    private Vector2 _squareSize;
+    private Vector2 _firstTouchedBoardPos;
+    
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _boardManager = BoardManager.Instance;
+    }
+
+    public void SetBoardSquareSize(Vector2 size)
+    {
+        _squareSize = size;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _firstTouchedBoardPos = GetTouchBoardPosition();
+            Debug.LogWarning("Touch x : " + _firstTouchedBoardPos.x + " y : " + _firstTouchedBoardPos.y);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var currentTouchPos = GetTouchBoardPosition();
+            if (currentTouchPos != _firstTouchedBoardPos)
+            {
+                Debug.LogWarning("Touch x : " + currentTouchPos.x + " y : " + currentTouchPos.y);
+            }
+        }
+    }
+
+    private Vector2Int GetTouchBoardPosition()
+    {
+        var touchPos = Input.mousePosition;
+        var offSet = touchPos - _boardManager.transform.position;
+        var lossyScale = _boardManager.transform.lossyScale;
+        return new Vector2Int( (int)((offSet.x / _squareSize.x) * (1f / lossyScale.x)), (int)((offSet.y / _squareSize.y) * (1f / lossyScale.y)));
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
     }
 }
